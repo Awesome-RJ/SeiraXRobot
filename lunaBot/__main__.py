b4d62231d0249e2526c8a6988f9b20d219e03d0b
seth <87837653+Dorimuhai@users.noreply.github.com>
2021-12-06 01:08:20 +0700

    Update __main__.py

--- lunaBot/__main__.py
+++ lunaBot/__main__.py
@@ -72,7 +72,7 @@ def get_readable_time(seconds: int) -> str:
 
     return ping_time
 
-LUNA_IMG = "https://telegra.ph/file/7c3c26e0ed938aec91209.jpg"
+LUNA_IMG = "https://telegra.ph/file/b05fb827470a67aeec05f.jpg"
 
 PM_START_TEXT = """
 ✪ ʜᴇʟʟᴏ  ᴍʏ ɴᴀᴍᴇ ɪs ꜱᴇɪʀᴀ ʀᴏʙᴏᴛ [🌠](https://telegra.ph/file/81e28997dcd6153979fa7.jpg)
