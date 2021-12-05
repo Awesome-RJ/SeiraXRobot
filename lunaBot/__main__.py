8e788d91df69d48dd22afccaf084a4c66434c2e8
seth <87837653+Dorimuhai@users.noreply.github.com>
2021-12-06 00:55:37 +0700

    Update __main__.py

--- lunaBot/__main__.py
+++ lunaBot/__main__.py
@@ -222,7 +222,7 @@ def start(update: Update, context: CallbackContext):
                 timeout=60,
             )
     else:
-        update.effective_message.reply_text(
+        update.effective_message.reply_photo(
            LUNA_IMG, caption= "I'm awake already!\n<b>Haven't sleep since:</b> <code>{}</code>".format(
                uptime
             ),
