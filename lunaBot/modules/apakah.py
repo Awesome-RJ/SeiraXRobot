e96595e22545deeb8d1ccd1eba66db7894c289cc
seth <87837653+Dorimuhai@users.noreply.github.com>
2021-12-06 11:02:55 +0700

    Create apakah.py

---
+++ lunaBot/modules/apakah.py
@@ -0,0 +1,25 @@
+import random
+from lunaBot.events import register
+from lunaBot.events import telethn
+
+APAKAH_STRING = ["Iya", 
+                 "Tidak", 
+                 "Mungkin", 
+                 "Mungkin Tidak", 
+                 "Bisa jadi", 
+                 "Mungkin Tidak",
+                 "Tidak Mungkin",
+                 "YNTKTS",
+                 "Pala bapak kau pecah",
+                 "Coba lagi",
+                 "Tanya aja sama mamak kau tu lah"
+                 ]
+
+
+@register(pattern="^/apakah ?(.*)")
+async def apakah(event):
+    quew = event.pattern_match.group(1)
+    if not quew:
+        await event.reply('Berikan Seira pertanyaan 😐')
+        return
+    await event.reply(random.choice(APAKAH_STRING))
