4b18334a6994f6d61cf0e84976d7f80f1850d605
riz-ex <90839716+riz-ex@users.noreply.github.com>
2021-12-09 13:32:55 +0700

    Update gtranslator.py

--- lunaBot/modules/gtranslator.py
+++ lunaBot/modules/gtranslator.py
@@ -1,26 +1,27 @@
-from gpytranslate import Translator
-from telegram.ext import CommandHandler, CallbackContext
+import json
+import os
+
+import requests
 from telegram import (
-    Message,
-    Chat,
-    User,
     ParseMode,
     Update,
-    InlineKeyboardMarkup,
     InlineKeyboardButton,
+    InlineKeyboardMarkup,
 )
-from lunaBot import dispatcher, pbot
-from pyrogram import filters
+from telegram.ext import CallbackContext, run_async
+from lunaBot import dispatcher
 from lunaBot.modules.disable import DisableAbleCommandHandler
+from gpytranslate import SyncTranslator
 
-trans = Translator()
 
+trans = SyncTranslator()
 
-@pbot.on_message(filters.command(["tl", "tr"]))
-async def translate(_, message: Message) -> None:
+@run_async
+def translate(update: Update, context: CallbackContext) -> None:
+    message = update.effective_message
     reply_msg = message.reply_to_message
     if not reply_msg:
-        await message.reply_text("Reply to a message to translate it!")
+        message.reply_text("Reply to a message to translate it!")
         return
     if reply_msg.caption:
         to_translate = reply_msg.caption
@@ -32,38 +33,44 @@ async def translate(_, message: Message) -> None:
             source = args.split("//")[0]
             dest = args.split("//")[1]
         else:
-            source = await trans.detect(to_translate)
+            source = trans.detect(to_translate)
             dest = args
     except IndexError:
-        source = await trans.detect(to_translate)
+        source = trans.detect(to_translate)
         dest = "en"
-    translation = await trans(to_translate, sourcelang=source, targetlang=dest)
+    translation = trans(to_translate, sourcelang=source, targetlang=dest)
     reply = (
         f"<b>Translated from {source} to {dest}</b>:\n"
         f"<code>{translation.text}</code>"
     )
 
-    await message.reply_text(reply, parse_mode="html")
+    message.reply_text(reply, parse_mode=ParseMode.HTML)
 
-__help__ = """ 
-Use this module to translate stuff!
-*Commands:*
-• /tl (or /tr): as a reply to a message, translates it to English.
-• /tl <lang>: translates to <lang>
-eg: /tl ja: translates to Japanese.
-• /tl <source>//<dest>: translates from <source> to <lang>.
-eg:  /tl ja//en: translates from Japanese to English.
+@run_async
+def languages(update: Update, context: CallbackContext) -> None:
+    update.effective_message.reply_text(
+        "Click on the button below to see the list of supported language codes.",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Language codes",
+                        url="https://telegra.ph/Lang-Codes-03-19-3",
+                    ),
+                ],
+            ],
+            disable_web_page_preview=True,
+        ),
+    )
 
-I can convert text to voice and voice to text..
-• /tts <lang code>*:* Reply to any message to get text to speech output
-• /stt*:* Type in reply to a voice message(support english only) to extract text from it.
-*Language Codes*
-`af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
-et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,
-id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,
-ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
-sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
-vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
-"""
 
-__mod_name__ = "Translator"
+TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], translate)
+LANGUAGE_HANDLER = DisableAbleCommandHandler(
+    ["lang", "langs"], languages
+)
+
+dispatcher.add_handler(TRANSLATE_HANDLER)
+dispatcher.add_handler(LANGUAGE_HANDLER)
+
+
+__command_list__ = ["tr", "tl"]
