9c227d2be4360d00169781190d21a4fcb727f786
seth <87837653+Dorimuhai@users.noreply.github.com>
2021-12-11 01:47:27 +0700

    Update afk.py

--- lunaBot/modules/afk.py
+++ lunaBot/modules/afk.py
@@ -58,14 +58,14 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                "{} is here!",
-                "{} is back!",
-                "{} is now in the chat!",
-                "{} is awake!",
-                "{} is back online!",
-                "{} is finally here!",
-                "Welcome back! {}",
-                "Where is {}?\nIn the chat!",
+                "{} Sudah tersedia !!",
+                "{} Kembali 😎",
+                "{} Telah hadir !",
+                "{} udah Kembali !",
+                "{} Sekarang online !",
+                "{} Akhirnya kembali!",
+                "Selamat gabut lagi ! {}",
+                "Dimana si {}?\nya di sini!",
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
@@ -129,10 +129,10 @@ def check_afk(update, context, user_id, fst_name, userc_id):
         if int(userc_id) == int(user_id):
             return
         if not user.reason:
-            res = "{} is afk".format(fst_name)
+            res = "{} sedang tidak tersedia".format(fst_name)
             update.effective_message.reply_text(res)
         else:
-            res = "{} is afk.\nReason: <code>{}</code>".format(
+            res = "{} sedang tidak tersedia.\nKarena: <code>{}</code>".format(
                 html.escape(fst_name), html.escape(user.reason)
             )
             update.effective_message.reply_text(res, parse_mode="html")
