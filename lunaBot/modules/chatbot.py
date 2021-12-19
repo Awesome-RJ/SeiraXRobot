281ea42f764111e0bb4c76057a94ea59c73e27ab
seth <87837653+Dorimuhai@users.noreply.github.com>
2021-12-19 23:13:27 +0700

    Update chatbot.py

--- lunaBot/modules/chatbot.py
+++ lunaBot/modules/chatbot.py
@@ -8,7 +8,7 @@ from pyrogram import filters
 from aiohttp import ClientSession
 from lunaBot import BOT_USERNAME as luna
 from lunaBot import BOT_ID, pbot, arq
-from lunaRobot.ex_plugins.chatbot import add_chat, get_session, remove_chat
+from LunaRobot.ex_plugins.chatbot import add_chat, get_session, remove_chat
 from lunaBot.utils.pluginhelper import admins_only, edit_or_reply
 
 url = "https://thearq.tech"
