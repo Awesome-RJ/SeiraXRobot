b02f28f01735c0e7db98127c0056341d72501716
seth <87837653+Dorimuhai@users.noreply.github.com>
2021-12-20 14:45:11 +0700

    Update mhelp.py

--- lunaBot/modules/mhelp.py
+++ lunaBot/modules/mhelp.py
@@ -7,23 +7,22 @@ from pyrogram import __version__ as pyrover
 from lunaBot.events import register as MEMEK
 from lunaBot import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/7c3c26e0ed938aec91209.jpg"
+PHOTO = "https://telegra.ph/file/1d196e1a45ac2e20178b7.jpg"
 
-@MEMEK(pattern=("/shelp"))
+@MEMEK(pattern=("/mhelp"))
 async def awake(event):
   tai = event.sender.first_name
-  LUNA = "** ──「 Basic Guide 」── ** \n\n"
-  LUNA += "• /play **(song title) — To Play the song you requested via YouTube** \n"
-  LUNA += "• /search ** (song/video title) – To search for links on YouTube with details** \n"
-  LUNA += "• /playlist - **show the list song in queue** \n"
-  LUNA += "•/lyric - ** (song name) lyrics scrapper** \n\n"
-  LUNA += "** ──「 Admin CMD 」── ** \n\n"
-  LUNA += "• /pause - **To Pause Song playback** \n"
-  LUNA += "• /resume - **To resume playback of the paused Song** \n"
-  LUNA += "• /skip - **To Skip playback of the song to the next Song** \n"
-  LUNA += "• /end - **To Stop Song playback** \n"
-  LUNA += "• /control - **open the player settings panel** \n"
-  LUNA += "• /reload - **To Refresh admin list** \n"
+  LUNA = "** Panduan Dasar \n\n"
+   LUNA += "• /vplay (judul lagu) — Untuk memutar video streaming yang Anda minta melalui YouTube** \n"
+   LUNA += "• /play (judul lagu/video) – Untuk memutar musik dari YouTube  \n"
+   LUNA += "• /vplaylist - menampilkan daftar video stream  dalam antrian \n"\n"
+   LUNA += "** Admin CMD \n\n"
+   LUNA += "• /pause - Untuk Menjeda playback Lagu** \n"
+   LUNA += "• /resume - Untuk melanjutkan playback Lagu yang dijeda \n"
+   LUNA += "• /skip - Untuk Melewati pemutaran lagu ke Lagu berikutnya \n"
+   LUNA += "• /end - Untuk Menghentikan playback Lagu \n"
+   LUNA += "• /musicplayer on - mengaktifkan music/video player di group anda \n"
+   LUNA += "• /musicplayer off - menonaktifkan music/video player di group anda \n"
 
-  BUTTON = [[Button.url("☎️ Support", "https://t.me/lunasupportgroup"), Button.url("📡 Updates", "https://t.me/ZeinzoProject")]]
+  BUTTON = [[Button.url("☎️ Support", "https://t.me/SeiraSupport"), Button.url("📡 Updates", "https://t.me/sethproject")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
