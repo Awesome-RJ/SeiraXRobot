ba4c780abaf6fb433d63aebb80b7b56e8dac4f32
seth <87837653+Dorimuhai@users.noreply.github.com>
2021-12-22 17:01:04 +0700

    Create Seth.py

---
+++ lunaBot/modules/Seth.py
@@ -0,0 +1,24 @@
+__mod_name__ = "Tools"
+
+_help_ = """
+*Tools yang bisa di gunakan di Serira* :
+★*Stickers*★
+• /getsticker <balas ke sticker> : mengubah sticker menjadi PNG file.
+• /kang <balas ke sticker> : membuat sticker pack anda lewat seira.
+• /stickers <masukan kata> : mencari sticker pack yang tersedia.
+• /q <balas ke pesan> : merubah pesan menjadi sticker.
+• /mmf <balas ke stiker> : menambahkan kalimat pada stiker,gunakan format " ; " untuk membuat tulisan dibawah.
+Contoh :
+[ ;Hai kak  ]
+
+★*Telegraph*
+• /tm <balas ke media atau sticker> : mengubah gambar atau sticker menjadi link telegraph .
+• /txt <balas ke pesan> : mengubah pesan menjadi link telegraph.
+
+★*Search*
+• /img <masukan kata> : mencari gambar di google.
+• /reverse <balas ke media atau sticker> : mencari gambar yang bersangkutan di google.
+• /app <nama aplikasi> : mencari aplikasi di play store.
+• /gps <masukan nama daerah> : mengirim lokasi via google maps.
+• /github <masukan nama github> : mengirim informasi user github.
+"""
