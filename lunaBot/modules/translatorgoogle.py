57baf3f2de5c7d7d1a69767f1d22aa1cf9948251
seth <87837653+Dorimuhai@users.noreply.github.com>
2021-12-23 19:58:20 +0700

    Create translatorgoogle.py

---
+++ lunaBot/modules/translatorgoogle.py
@@ -0,0 +1,26 @@
+__mod_name__ = "Translate"
+
+__help__ = """
+★Translate★
+Berikut adalah bantuan untuk modul Translate:
+Seira dapat mengubah teks menjadi suara dan suara menjadi teks..
+ 
+ Gunakan modul ini untuk menerjemahkan kata!
+ Perintah:
+• /tr <kode bahasa > <balas ke pesan> : menerjemahkannya ke bahasa yang ingin di translate.
+ - misalnya: /tr id <balas ke pesan> : diterjemahkan ke bahasa Indonesia.
+ - misalnya: /tr id//en: menerjemahkan dari bahasa Indonesia ke bahasa Inggris.
+
+• /langs: dapatkan daftar bahasa yang didukung untuk terjemahan.
+ 
+• /tts : Membalas pesan apa pun untuk mendapatkan keluaran teks ke ucapan.
+ - misalnya: /tts id <balas ke pesan> : mendapatkan keluaran teks ke ucapan Indonesia.
+ 
+*Kode Bahasa* :
+ af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
+ et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hai,hmn,hr,ht,hu,hy,
+ id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,
+ ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
+ sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
+ vi,xh,yi,yo,zh,zh_CN,zh_TW,zu
+"""
