f52a77e681611c32723face62288b1c488176fa2
seth <87837653+Dorimuhai@users.noreply.github.com>
2021-12-23 19:33:10 +0700

    Update __texttospeech.py

--- lunaBot/modules/__texttospeech.py
+++ lunaBot/modules/__texttospeech.py
@@ -1,18 +1,16 @@
+from lunaBot import telethn as tbot
 import os
-import urllib.request
-from datetime import datetime
-from typing import List
-from typing import Optional
-import requests
+
+from gtts import gTTS
+from gtts import gTTSError
 from telethon import *
-from telethon import events
 from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
 from lunaBot import *
 
-from lunaBot import telethn as tbot
+from lunaBot.events import register
 
 
 async def is_register_admin(chat, user):
@@ -27,63 +25,51 @@ async def is_register_admin(chat, user):
         return True
 
 
-@register(pattern="^/tts$")
+@register(pattern="^/tts (.*)")
 async def _(event):
     if event.fwd_from:
         return
-    start = datetime.now()
-    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
+    if event.is_group:
+     if not (await is_register_admin(event.input_chat, event.message.sender_id)):
+       await event.reply("🚨 Need Admin Power.. You can't use this command.. But you can use in my pm")
+       return
 
+    input_str = event.pattern_match.group(1)
+    reply_to_id = event.message.id
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
-        required_file_name = await event.client.download_media(
-            previous_message, TEMP_DOWNLOAD_DIRECTORY
-        )
-        if IBM_WATSON_CRED_URL is None or IBM_WATSON_CRED_PASSWORD is None:
-            await event.reply(
-                "You need to set the required ENV variables for this module. \nModule stopping"
-            )
-        else:
-            # await event.reply("Starting analysis")
-            headers = {
-                "Content-Type": previous_message.media.document.mime_type,
-            }
-            data = open(required_file_name, "rb").read()
-            response = requests.post(
-                IBM_WATSON_CRED_URL + "/v1/recognize",
-                headers=headers,
-                data=data,
-                auth=("apikey", IBM_WATSON_CRED_PASSWORD),
-            )
-            r = response.json()
-            if "results" in r:
-                # process the json to appropriate string format
-                results = r["results"]
-                transcript_response = ""
-                transcript_confidence = ""
-                for alternative in results:
-                    alternatives = alternative["alternatives"][0]
-                    transcript_response += " " + str(alternatives["transcript"])
-                    transcript_confidence += (
-                        " " + str(alternatives["confidence"]) + " + "
-                    )
-                end = datetime.now()
-                ms = (end - start).seconds
-                if transcript_response != "":
-                    string_to_show = "TRANSCRIPT: `{}`\nTime Taken: {} seconds\nConfidence: `{}`".format(
-                        transcript_response, ms, transcript_confidence
-                    )
-                else:
-                    string_to_show = "TRANSCRIPT: `Nil`\nTime Taken: {} seconds\n\n**No Results Found**".format(
-                        ms
-                    )
-                await event.reply(string_to_show)
-            else:
-                await event.reply(r["error"])
-            # now, remove the temporary file
-            os.remove(required_file_name)
+        text = previous_message.message
+        lan = input_str
+    elif "|" in input_str:
+        lan, text = input_str.split("|")
     else:
-        await event.reply("Reply to a voice message, to get the text out of it.")
-
-__mod_name__ = "TTS/STT"
+        await event.reply(
+            "Invalid Syntax\nFormat `/tts lang | text`\nFor eg: `/tts en | hello`"
+        )
+        return
+    text = text.strip()
+    lan = lan.strip()
+    try:
+        tts = gTTS(text, tld="com", lang=lan)
+        tts.save("k.mp3")
+    except AssertionError:
+        await event.reply(
+            "The text is empty.\n"
+            "Nothing left to speak after pre-precessing, "
+            "tokenizing and cleaning."
+        )
+        return
+    except ValueError:
+        await event.reply("Language is not supported.")
+        return
+    except RuntimeError:
+        await event.reply("Error loading the languages dictionary.")
+        return
+    except gTTSError:
+        await event.reply("Error in Google Text-to-Speech API request !")
+        return
+    with open("k.mp3", "r"):
+        await tbot.send_file(
+            event.chat_id, "k.mp3", voice_note=True, reply_to=reply_to_id
+        )
+        os.remove("k.mp3")
